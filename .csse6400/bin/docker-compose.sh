8ae526b8f9ec526daa6483570aed4d3e68183c62
Nawal <n.ahmednazim@uqconnect.edu.au>
2025-03-20 12:50:59 +1000

    renamed docker-compose

--- .csse6400/bin/docker-compose.sh
+++ .csse6400/bin/docker-compose.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #
 # Check that the health endpoint is returning 200 using docker-compose
-docker-compose build
-docker-compose up -d
+docker compose build
+docker compose up -d
 error=$?
 pid=$!
 if [[ $error -ne 0 ]]; then
-    echo "Failed to run docker-compose up"
+    echo "Failed to run docker compose up"
     exit 1
 fi
 
@@ -21,5 +21,5 @@ if [[ $error -ne 0 ]]; then
     exit 1
 fi
 
-docker-compose down
+docker compose down
 
